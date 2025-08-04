00b8988f81702088995d49ae8bf727be326895ee
RINUMATHEWREJI <46rinu@gmail.com>
2025-08-05 00:30:40 +0530

    Fix CORS for deployed frontend

--- server/server.js
+++ server/server.js
@@ -6,9 +6,7 @@ const {ConnectDb} = require('./utils/ConnectDb');
 const Taskrouter = require('./routers/Task.router');
 
 const app = express();
-app.use(cors({
-    origin:'http://localhost:5173'
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/tasks',Taskrouter);
 
